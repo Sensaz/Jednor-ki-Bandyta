556e241120cd4d888314e10e8afb3712e86035df
Sensaz <klaudiusz.wiktor1@gmail.com>
2022-02-27 19:43:26 +0100

    Jednoręki Bandyta OOP

---
+++ Wallet.js
@@ -0,0 +1,27 @@
+class Wallet {
+    constructor(money) {
+        let _money = money;
+        // Pobieranie aktualnej zawartości portfela
+        this.getWalletValue = () => _money;
+
+        // Sprawdzanie czy użytkownik ma odpowiednią ilość środków
+        this.checkCanPlay = value => {
+            if (_money >= value) return true;
+            return false;
+        }
+        
+        this.changeWallet = (value, type = '+') => {
+            if (typeof value === 'number' && !isNaN(value)) {
+                if (type === '+') {
+                    return _money += value;
+                } else if (type === '-') {
+                    return _money -= value
+                } else {
+                    throw new Error('nieprwaidłowy typ działania')
+                }
+            } else {
+                throw new Error('nieprwaidłowa liczba')
+            }
+        }
+    }
+}
